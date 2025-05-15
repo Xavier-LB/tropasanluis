5a1a2ec3fa7a48250bc649be0a6d908e10d0e84b
Xavier Le Baux <xavier.lebaux@gmail.com>
2025-05-15 11:10:03 -0400

    Add yt and spotify, and update lucho-cuentero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -132,6 +132,30 @@
             <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/>
           </svg>
         </a>
+        
+        <a 
+          href="https://www.youtube.com/channel/YOUR_CHANNEL_ID" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          class="text-[#C1272D] hover:text-[#A11F25] transition transform hover:-translate-y-1 hover:scale-110"
+          aria-label="YouTube"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
+          </svg>
+        </a>
+        
+        <a 
+          href="https://open.spotify.com/user/YOUR_SPOTIFY_ID" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          class="text-[#C1272D] hover:text-[#A11F25] transition transform hover:-translate-y-1 hover:scale-110"
+          aria-label="Spotify"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z"/>
+          </svg>
+        </a>
       </div>
     </div>
 
