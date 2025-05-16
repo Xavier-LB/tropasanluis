f22d665025aebef75fc61cd8a85d333111027bbb
Xavier Le Baux <xavier.lebaux@gmail.com>
2025-05-16 18:04:52 -0400

    add x

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -134,7 +134,19 @@
         </a>
         
         <a 
-          href="https://www.youtube.com/channel/YOUR_CHANNEL_ID" 
+          href="https://x.com/TropaSanLuis" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          class="text-[#C1272D] hover:text-[#A11F25] transition transform hover:-translate-y-1 hover:scale-110"
+          aria-label="X (Twitter)"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/>
+          </svg>
+        </a>
+        
+        <a 
+          href="https://www.youtube.com/@tropasanluis7517" 
           target="_blank" 
           rel="noopener noreferrer"
           class="text-[#C1272D] hover:text-[#A11F25] transition transform hover:-translate-y-1 hover:scale-110"
